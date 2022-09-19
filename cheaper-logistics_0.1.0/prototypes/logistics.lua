6161569b16792ce89efe0001df75405ef94d30cb
SnipSnipSnipered <99442665+SnipSnipSnipered@users.noreply.github.com>
2022-09-20 00:12:09 +0200

    Added settings.

--- cheaper-logistics_0.1.0/prototypes/logistics.lua
+++ cheaper-logistics_0.1.0/prototypes/logistics.lua
@@ -1,74 +1,84 @@
---Logistics
-data.raw["recipe"]["transport-belt"].ingredients =
-{
-    {"iron-plate", 1},
-    {"iron-gear-wheel", 1},
-}
-
-data.raw["recipe"]["underground-belt"].ingredients =
-{
-    {"transport-belt", 5},
-    {"iron-plate", 4},
-}
-
-data.raw["recipe"]["splitter"].ingredients = 
-{
-    {"transport-belt", 2},
-    {"electronic-circuit", 2},
-    {"iron-plate", 2},
-}
-
---Fast logistics
-data.raw["recipe"]["fast-transport-belt"].ingredients =
-{
-    {"iron-gear-wheel", 6},
-    {"transport-belt", 2},
-}
-data.raw["recipe"]["fast-transport-belt"].result_count = 2
-
-data.raw["recipe"]["fast-underground-belt"].ingredients =
-{
-    {"fast-transport-belt", 7},
-    {"iron-plate", 4},
-}
-
-data.raw["recipe"]["fast-splitter"].ingredients = 
-{
-    {"fast-transport-belt", 2},
-    {"iron-gear-wheel", 4},
-    {"electronic-circuit", 4},
-}
-
---Express logistics
-data.raw["recipe"]["express-transport-belt"].ingredients =
-{
-    {"iron-gear-wheel", 12},
-    {"fast-transport-belt", 2},
-    {type="fluid", name="lubricant", amount=40},
-}
-data.raw["recipe"]["express-transport-belt"].result_count = 2
-
-data.raw["recipe"]["express-underground-belt"].ingredients =
-{
-    {"express-transport-belt", 9},
-    {"iron-plate", 4},
-    {type="fluid", name="lubricant", amount=80},
-}
-
-data.raw["recipe"]["express-splitter"].ingredients =
-{
-    {"express-transport-belt", 2},
-    {"iron-gear-wheel", 10},
-    {"advanced-circuit", 5},
-    {type="fluid", name="lubricant", amount=80},
-}
-
-table.insert(data.raw["technology"]["stone-wall"].effects =
-{
-    data.raw["wall"]["stone-wall"].resistances = 
+--Logistics 1
+    data.raw["recipe"]["underground-belt"].ingredients =
     {
-        type = "explosion",
-        percent = 100
+        {"transport-belt", 5},
+        {"iron-plate", 4},
     }
-}
-)
+    data.raw["recipe"]["splitter"].ingredients = 
+    {
+        {"transport-belt", 2},
+        {"electronic-circuit", 2},
+        {"iron-plate", 2},
+    }
+--Logistics 2 & 3
+    data.raw["recipe"]["fast-transport-belt"].ingredients =
+    {
+        {"iron-gear-wheel", 6},
+        {"transport-belt", 2},
+    }
+    data.raw["recipe"]["fast-transport-belt"].result_count = 2
+
+    data.raw["recipe"]["express-transport-belt"].ingredients =
+    {
+        {"iron-gear-wheel", 12},
+        {"fast-transport-belt", 2},
+        {type="fluid", name="lubricant", amount=40},
+    }
+    data.raw["recipe"]["express-transport-belt"].result_count = 2
+
+--Require previous tiers?
+if settings.startup["cl-req-prev"].value == true then 
+    data.raw["recipe"]["fast-underground-belt"].ingredients =
+    {
+        {"fast-transport-belt", 7},
+        {"underground-belt", 2},
+    }
+    data.raw["recipe"]["fast-splitter"].ingredients = 
+    {
+        {"splitter", 1},
+        {"iron-gear-wheel", 4},
+        {"electronic-circuit", 4},
+    }
+    data.raw["recipe"]["express-underground-belt"].ingredients =
+    {
+        {"express-transport-belt", 9},
+        {"fast-underground-belt", 2},
+        {type="fluid", name="lubricant", amount=80},
+    }
+
+    data.raw["recipe"]["express-splitter"].ingredients =
+    {
+        {"fast-splitter", 1},
+        {"iron-gear-wheel", 10},
+        {"advanced-circuit", 5},
+        {type="fluid", name="lubricant", amount=80},
+    }
+end
+
+if settings.startup["cl-req-prev"].value == false then
+    data.raw["recipe"]["fast-underground-belt"].ingredients =
+    {
+        {"fast-transport-belt", 7},
+        {"iron-plate", 4},
+    }
+    data.raw["recipe"]["fast-splitter"].ingredients = 
+    {
+        {"fast-transport-belt", 2},
+        {"iron-gear-wheel", 4},
+        {"electronic-circuit", 4},
+    }
+    data.raw["recipe"]["express-underground-belt"].ingredients =
+    {
+        {"express-transport-belt", 9},
+        {"iron-plate", 4},
+        {type="fluid", name="lubricant", amount=80},
+    }
+
+    data.raw["recipe"]["express-splitter"].ingredients =
+    {
+        {"express-transport-belt", 2},
+        {"iron-gear-wheel", 10},
+        {"advanced-circuit", 5},
+        {type="fluid", name="lubricant", amount=80},
+    }
+end   
---
+++ cheaper-logistics_0.1.0/settings.lua
@@ -0,0 +1,8 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "cl-req-prev",
+        setting_type = "runtime-global",
+        default_value = false
+    }
+})
