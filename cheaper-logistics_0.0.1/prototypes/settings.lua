544c39c9ab30db59d83a0bfcacc79f378089605d
SnipSnipSnipered <99442665+SnipSnipSnipered@users.noreply.github.com>
2022-09-19 21:21:58 +0200

    Added settings.lua with several settings for managing recipes.

---
+++ cheaper-logistics_0.0.1/prototypes/settings.lua
@@ -0,0 +1,23 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "underground-requires-previous",
+        setting_type = "startup",
+        default_value = false,
+        order = "a"
+    },
+    {
+        type = "bool-setting",
+        name = "splitters-requires-previous",
+        setting_type = "startup",
+        default_value = false,
+        order = "b"
+    },
+    {
+        type = "bool-setting",
+        name = "enable-cheaper-underground-pipes",
+        setting_type = "startup",
+        default_value = true,
+        order = "c"
+    }
+})
